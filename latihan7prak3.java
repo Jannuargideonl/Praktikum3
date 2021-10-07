ffe10c17d046e83bd47462e1538d5f2b916b790b
Jannuargideonl <91230108+Jannuargideonl@users.noreply.github.com>
2021-10-07 12:31:39 +0800

    Add files via upload

---
+++ latihan7prak3.java
@@ -0,0 +1,12 @@
+// Latihan mencetak angka berurut
+
+public class latihan7prak3 {
+    public static void main(String args[]){
+        int i; 
+        for (i=1; i<9; i++) 
+        {
+        System.out.println(i);
+
+}
+}
+}
